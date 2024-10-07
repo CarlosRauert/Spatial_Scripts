a4b9752657e881b2a6e0872aec76230cbd3aed99
CarlosRauert <carlos.rauert@charite.de>
2024-10-07 20:37:31 +0200

    Slurm

--- CellCharter/20241007_NicheDE_Slurm.R
+++ CellCharter/20241007_NicheDE_Slurm.R
@@ -8,7 +8,7 @@ library(Seurat)
 
 NDE_obj_m <- readRDS('/data/cephfs-2/unmirrored/projects/liposarcoma-wgs/Niche_DE_Rang20/20241007_NicheDE/20241007NDEobj_m_postEffniche.rds')
 
-NDE_obj_m = niche_DE(NDE_obj_m,num_cores = 32, outfile = "",C = 30, M = 10, gamma = 0.8,print = T, Int = T, batch = T,self_EN = F,G = 1)
+NDE_obj_m = niche_DE(NDE_obj_m,num_cores = 12, outfile = "",C = 30, M = 10, gamma = 0.8,print = T, Int = T, batch = T,self_EN = F,G = 1)
 
 saveRDS(NDE_obj, '/data/cephfs-2/unmirrored/projects/liposarcoma-wgs/Niche_DE_Rang20/20241007_NicheDE/20241007NDEobj_m_postNDE.rds')
 
--- CellCharter/20241007_Slurm.sh
+++ CellCharter/20241007_Slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#SBATCH --mem=2000G
-#SBATCH --ntasks=32
+#SBATCH --mem=800G
+#SBATCH --ntasks=12
 #SBATCH --time=2-00:00:00
 
 conda init
