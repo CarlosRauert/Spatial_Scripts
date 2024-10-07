dc95303336cbb155fecbb5771946ea61979f65e7
CarlosRauert <carlos.rauert@charite.de>
2024-10-07 18:51:47 +0200

    added slurm

---
+++ CellCharter/20241007_Slurm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+#SBATCH --mem=2000G
+#SBATCH --ntasks=32
+#SBATCH --time=2-00:00:00
+
+conda init
+conda activate NicheDE
+
+$HOME/work/Scripts_Git_Repos/Spatial_Scripts/CellCharter/20241007_NicheDE_Slurm.R
