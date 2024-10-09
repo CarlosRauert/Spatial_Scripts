48cfd36520c87b20e32e9f95e10b610e460ffa44
CarlosRauert <carlos.rauert@charite.de>
2024-10-09 13:54:57 +0200

    fee

---
+++ LPS_scRNAseq/Prefetch_SRA_List.sh
@@ -0,0 +1,1 @@
+prefetch --option-file /data/cephfs-1/scratch/groups/dubois/users/rauertc_c/scRNAseq_LPS/SRR_Acc_List.txt --output-directory /data/cephfs-1/scratch/groups/dubois/users/rauertc_c/scRNAseq_LPS/SRA
